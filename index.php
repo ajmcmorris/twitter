1349258ef0542a01bbdf73f242f7ab4f082bdccf
jordan <ajmcmorris@gmail.com>
2015-07-15 11:44:49 -0400

    updated index.php

--- index.php
+++ index.php
@@ -18,8 +18,15 @@ $string = json_decode($twitter->setGetfield($getfield)
 ->buildOauth($url, $requestMethod)
 ->performRequest(),$assoc = TRUE);
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-echo "<pre>";
-print_r($string);
-echo "</pre>";
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }
 
 ?>
